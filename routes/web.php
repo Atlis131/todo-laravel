e1a7e6ccfe18605cd7498f68b42a14bb61bb83fd
Atlis <atlis131@gmail.com>
2024-12-21 20:18:20 +0100

    Small Fixes

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@ Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.pos
 Route::get('registration', [AuthController::class, 'registration'])->name('register');
 Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post');
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
-Route::get('dashboard', [TaskController::class, 'index'])->name('register');
+Route::get('dashboard', [TaskController::class, 'index'])->name('dashboard');
 Route::resource('tasks', TaskController::class);
